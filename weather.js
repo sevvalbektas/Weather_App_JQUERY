273f85c169cf8e272f7a7da736a25f3c4783af5f
Şevval Bektaş <sevvalbektas5561@gmail.com>
2022-10-04 22:04:28 +0300

    second commit

--- weather.js
+++ weather.js
@@ -1,58 +1,112 @@
 const formJS = document.querySelectorAll("form")[0];
-
-// const formJquery = jQuery("form").eq(0); 
-// const formJquery = jQuery("form")[0]; 
-//* ilk form ile ilgilenmek için 0. indeks alındı
-//* ama jquery objesi olarak değil js objesi olarak ulaşırız. Bu sebeple indisleme ile yapamayız. get(index) veya eq(index) kullanarak yapabiliriz.
-//* jQERY === $
+//jQUERY === $
+//const formJquery = jQuery("form").eq(0);
 const formJquery = $("form").eq(0);
 const inputJQ = $(".top-banner input").eq(0);
 const msgJQ = $(".top-banner span").eq(0);
 const listJQ = $(".cities").eq(0);
 
+//console.log(formJS);
+//console.log(formJquery);
+//console.log(inputJQ);
 
-// console.log(formJS);
-// console.log(formJquery);
-// console.log(inputJQ);
-// console.log(msgJQ);
-// console.log(listJQ);
+// get(index) ==> toArray(get()) , eq(index) 
 
-//* get(index) ==> toArray(get()), eq(index) 
+//load VS DOMContentLoaded
+//DOMContentLoaded ==> means page rendered, DOM is ready
+//window load ==> (all content (e.g. images, styles etc) also loaded)
 
-//* load VS DOMContentLoaded
-//* DOMContentLoaded dom tree oluşur ve sayfanın render edildiği anlamına gelir
+//window.onload = () =>{} ===> JS
+// addEventListener ===> on
 
-//* load VS DOMContentLoaded
-//* DOMContentLoaded ==> means page rendered, DOM is ready
-//* window load ==> (all content (e.g. images, styles etc) also loaded)
-
-//* window.onload = () => {}  ===> JS
-//* addEventListener ===> on
-
-$(window).on("load", () => {
+$(window).on("load", ()=>{
     console.log("window.load");
 });
 
-//* document.addEventListener("DOMContentLoaded", ()=>{})  ===> JS
-// $(document).on(() => {
-//     console.log("DOMContentLoaded");  //* window.load'dan önce çalışır.
+// document.addEventListener("DOMContentLoaded", ()=>{}) ==> JS
+
+// $(document).on("DOMContentLoaded", ()=>{
+//     console.log("DOMContentLoaded");
 // });
-$(document).ready(() => {
-    console.log("DOMContentLoaded");  //* window.load'dan önce çalışır.
+
+$(document).ready(()=>{
+    console.log("DOMContentLoaded");
+    localStorage.setItem("apiKey", EncryptStringAES("4d8fb5b93d4af21d66a2948710284366"));
 });
 
-// formJquery.on("submit", (e) => {
+// formJquery.on("submit", (e)=>{
 //     e.preventDefault();
 //     getWeatherDataFromApi();
 // });
 
-formJquery.submit((e) => {
+formJquery.submit((e)=>{
     e.preventDefault();
     getWeatherDataFromApi();
 });
 
-const getWeatherDataFromApi = () => {
-    console.log("AJAX Func. is called");
+
+const getWeatherDataFromApi = async() =>{
+    //console.log("AJAX Func. is called");
+    const apiKey = DecryptStringAES(localStorage.getItem("apiKey"));
+    //JS .value == jQUERY .val()
+    const cityName = inputJQ.val();
+    console.log(cityName);
+    const units = "metric";
+    const lang = "tr";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=${units}&lang=${lang}`;
+
+    // XMLHttpRequest(xhr) vs. fetch() vs. axios vs. $.ajax
+
+    $.ajax({
+        type : "GET",
+        url : url,
+        dataType : "json",
+        success: (response) => {
+            //main body func.
+            console.log(response);
+            const { main, sys, name, weather } = response;
+            const iconUrlAWS = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0].icon}.svg`;
+            //alternative iconUrl
+            const iconUrl = `https://openweathermap.org/img/wn/${weather[0].icon}@2x.png`;
+
+            //js=>document.createElement("li")
+
+            // const createdLi2 = $(document.createElement("li"))
+
+            //weather card control!!
+
+            
+            const createdLi = $('<li></li>');
+            createdLi.addClass("city");
+            createdLi.html(`
+            <h2 class="city-name" data-name="${name}, ${sys.country}">
+                <span>${name}</span>
+                <sup>${sys.country}</sup>
+            </h2>
+            <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
+            <figure>
+                <img class="city-icon" src="${iconUrl}">
+                <figcaption>${weather[0].description}</figcaption>
+            </figure>`);
+
+            //append vs. prepend both in JS AND JQUERY
+            listJQ.prepend(createdLi);
+            //formJS.reset();
+            formJquery.trigger("reset");
+
+        },
+        beforeSend: (request) => {
+            console.log("before ajax send");
+        },
+        complete: ()=>{
+            console.log("after ajax send");
+        },
+        error:(XMLHttpRequest) =>{
+            console.log(XMLHttpRequest);
+        }
+    });
+
+    
+
 }
 
-//* XMLHTTPREQUEST(xhr) vs. fetch() vs. axios vs. $.ajax
